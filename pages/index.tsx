2eb51b065e09b29854a600192f2ae4691954a146
Matthias Berchtold <matthi.berchtold@gmail.com>
2022-04-14 00:03:54 +0200

    feat: add help and echo commands

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,222 @@
+import { KeyboardEvent, useState } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import {
+  VscChromeClose,
+  VscChromeMaximize,
+  VscChromeMinimize,
+} from 'react-icons/vsc'
+import { TiArrowRightThick } from 'react-icons/ti'
+import { parse as cmdParse, ParseEntry } from 'shell-quote'
+import getopts from 'getopts'
+
+type History = {
+  command: string
+  result: JSX.Element | string
+  path: string
+  timestamp: string
+}
+
+enum GetoptsType {
+  string = 'string',
+  boolean = 'boolean',
+}
+
+type Option = {
+  name: string
+  description: string
+  getoptsType: GetoptsType
+}
+
+type Operand = {
+  name: string
+  description: string
+}
+
+type Command = {
+  name: string
+  description: string
+  operands: Operand[]
+  options: Option[]
+  handler: (args: getopts.ParsedOptions) => JSX.Element | string
+}
+
+const commands: Command[] = [
+  {
+    name: 'help',
+    description: 'print help',
+    operands: [],
+    options: [],
+    handler: (args) => {
+      const help = (
+        <>
+          {commands.map((command) => (
+            <div key={command.name}>
+              <span>
+                {command.name} - {command.description}
+              </span>
+              {/* {command.operands.map((operand) => (
+                <>
+                  <span className="ml-4">
+                    {operand.name} - {operand.description}
+                  </span>
+                </>
+              ))}
+              {command.options.map((option) => (
+                <>
+                  <span className="ml-4">
+                    {option.name} - {option.description}
+                  </span>
+                </>
+              ))}
+              <br /> */}
+            </div>
+          ))}
+        </>
+      )
+      return help
+    },
+  },
+  {
+    name: 'ls',
+    description: 'list directory contents',
+    operands: [],
+    options: [
+      {
+        name: 'l',
+        description: 'use a long listing format',
+        getoptsType: GetoptsType.boolean,
+      },
+    ],
+    handler: (args) => {
+      return 'list all'
+    },
+  },
+  {
+    name: 'echo',
+    description: 'display a line of text',
+    operands: [
+      {
+        name: 'text',
+        description: 'the text to display',
+      },
+    ],
+    options: [],
+    handler: (args) => {
+      return args._[0]
+    },
+  },
+]
 
 const Home: NextPage = () => {
+  const [history, setHistory] = useState<History[]>([
+    {
+      command: 'help',
+      result: commands[0].handler({ _: [] }),
+      path: '~',
+      timestamp: '2020-01-01T00:00:00.000Z',
+    },
+  ])
+
+  const cmd = (input: string) => {
+    const [command, ...args] = cmdParse(input)
+    const commandObj = commands.find((c) => c.name === command)
+    if (commandObj) {
+      const rules: getopts.Options = commandObj.options.reduce((prev, curr) => {
+        if (curr.getoptsType in prev) prev[curr.getoptsType].push(curr.name)
+        else prev[curr.getoptsType] = [curr.name]
+        return prev
+      }, {} as { [key in GetoptsType]: string[] })
+
+      const paramObj = getopts(
+        args.map((entry: ParseEntry) => entry.toString()),
+        rules
+      )
+
+      return commandObj.handler(paramObj)
+    }
+    return 'command not found: ' + command
+  }
+
+  const getPathSymbol = (path: string) => {
+    return path
+  }
+
+  const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      const command = event.currentTarget.value
+      const result = cmd(command)
+      setHistory([
+        ...history,
+        { command, result, path: '~', timestamp: new Date().toISOString() },
+      ])
+      event.currentTarget.value = ''
+    }
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
-        <title>Create Next App</title>
+        <title>Matthias Berchtold</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+      <main className="flex w-full flex-1 flex-col items-center justify-center bg-black px-20 text-center">
+        <div
+          className="text-s flex flex-col text-white"
+          style={{ width: '735px', height: '480px' }}
+        >
+          <div
+            className="relative flex w-full cursor-default items-center justify-center rounded-t-lg bg-zinc-800 p-5"
+            style={{ height: '50px' }}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <span>matthias@portfolio:~</span>
+            <div className="absolute right-0 flex flex-row gap-8 p-5">
+              <button>
+                <VscChromeMinimize />
+              </button>
+              <button>
+                <VscChromeMaximize />
+              </button>
+              <button>
+                <VscChromeClose />
+              </button>
+            </div>
+          </div>
+          <div className="w-full grow cursor-text overflow-auto bg-zinc-700 px-1 py-2">
+            {history.map((command) => (
+              <div
+                className="flex flex-col items-start"
+                key={command.timestamp}
+              >
+                <div className="flex items-center gap-2">
+                  <TiArrowRightThick color="green" />
+                  <span className="text-blue-500">
+                    {getPathSymbol(command.path)}
+                  </span>
+                  <span>{command.command}</span>
+                </div>
+                <div className="flex flex-col items-start justify-start">
+                  {command.result}
+                </div>
+              </div>
+            ))}
+            <div className="flex items-center gap-2">
+              <TiArrowRightThick color="green" />
+              <span className="text-blue-500">~</span>
+              <div className="relative grow">
+                <input
+                  className="w-full border-0 bg-transparent outline-0"
+                  onKeyPress={handleKeyPress}
+                ></input>
+                {/* <i className="caret"></i> */}
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   )
 }
