2cdb689ac207602127da306652f094947d524e70
Matthias Berchtold <matthi.berchtold@gmail.com>
2022-08-17 14:34:59 +0200

    feat: remove unnecessary files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,6 @@ import Head from '@components/Head'
 import Layout from '@components/Layout'
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
-import Head from '../components/Head'
-import { OperatingSystem } from '../components/OperatingSystem'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
